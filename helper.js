83666f664fdecdff7d4ddb8aaef00a5d1ac9aa7a
Rasoul Ahmadifar <r.ahmadifar.1377@gmail.com>
2022-06-06 18:19:44 +0430

    remove wait selector

--- helper.js
+++ helper.js
@@ -215,19 +215,14 @@ const doTask = async (page, task, tags) => {
             if (follow) {
                 actions.push('follow');
 
-                if (await page.waitForSelector('[aria-label="Follow @' + task_username + '"]', { visible: true, timeout: 10000 }).catch((e) => { return 'error' }) === 'error') {
+                if (await page.click('[aria-label="Follow @' + task_username + '"]').catch((e) => { return 'error' }) === 'error') {
                     if (await page.waitForSelector('[aria-label="Following @' + task_username + '"]', { visible: true, timeout: 10000 }).catch((e) => { return 'error' }) === 'error') {
                         fails.push('follow');
                     }
                 } else {
-
-                    if (await page.click('[aria-label="Follow @' + task_username + '"]').catch((e) => { return 'error' }) === 'error') {
+                    await page.waitForTimeout(500);
+                    if (await page.waitForSelector('[aria-label="Following @' + task_username + '"]', { visible: true, timeout: 10000 }).catch((e) => { return 'error' }) === 'error') {
                         fails.push('follow');
-                    } else {
-                        await page.waitForTimeout(500);
-                        if (await page.waitForSelector('[aria-label="Following @' + task_username + '"]', { visible: true, timeout: 10000 }).catch((e) => { return 'error' }) === 'error') {
-                            fails.push('follow');
-                        }
                     }
                 }
                 await page.waitForTimeout(1000);
@@ -236,18 +231,14 @@ const doTask = async (page, task, tags) => {
             if (like) {
                 actions.push('like');
 
-                if (await page.waitForSelector('[aria-label="Like"]', { visible: true, timeout: 10000 }).catch((e) => { return 'error' }) === 'error') {
+                if (await page.click('[aria-label="Like"]').catch((e) => { return 'error' }) === 'error') {
                     if (await page.waitForSelector('[aria-label="Liked"]', { visible: true, timeout: 10000 }).catch((e) => { return 'error' }) === 'error') {
                         fails.push('like');
                     }
                 } else {
-                    if (await page.click('[aria-label="Like"]').catch((e) => { return 'error' }) === 'error') {
+                    await page.waitForTimeout(500);
+                    if (await page.waitForSelector('[aria-label="Liked"]', { visible: true, timeout: 10000 }).catch((e) => { return 'error' }) === 'error') {
                         fails.push('like');
-                    } else {
-                        await page.waitForTimeout(500);
-                        if (await page.waitForSelector('[aria-label="Liked"]', { visible: true, timeout: 10000 }).catch((e) => { return 'error' }) === 'error') {
-                            fails.push('like');
-                        }
                     }
                 }
                 await page.waitForTimeout(1000);
@@ -256,22 +247,18 @@ const doTask = async (page, task, tags) => {
             if (retweet) {
                 actions.push('retweet')
 
-                if (await page.waitForSelector('[aria-label="Retweet"]', { visible: true, timeout: 10000 }).catch((e) => { return 'error' }) === 'error') {
+                if (await page.click('[aria-label="Retweet"]').catch((e) => { return 'error' }) === 'error') {
                     if (await page.waitForSelector('[aria-label="Retweeted"]', { visible: true, timeout: 10000 }).catch((e) => { return 'error' }) === 'error') {
                         fails.push('retweet');
                     }
                 } else {
-                    if (await page.click('[aria-label="Retweet"]').catch((e) => { return 'error' }) === 'error') {
+                    await page.waitForTimeout(1000);
+                    await page.keyboard.press('ArrowDown');
+                    await page.keyboard.press('ArrowDown');
+                    await page.keyboard.press('Enter');
+                    await page.waitForTimeout(1000);
+                    if (await page.waitForSelector('[aria-label="Retweeted"]', { visible: true, timeout: 10000 }).catch((e) => { return 'error' }) === 'error') {
                         fails.push('retweet');
-                    } else {
-                        await page.waitForTimeout(1000);
-                        await page.keyboard.press('ArrowDown');
-                        await page.keyboard.press('ArrowDown');
-                        await page.keyboard.press('Enter');
-                        await page.waitForTimeout(1000);
-                        if (await page.waitForSelector('[aria-label="Retweeted"]', { visible: true, timeout: 10000 }).catch((e) => { return 'error' }) === 'error') {
-                            fails.push('retweet');
-                        }
                     }
                 }
                 await page.waitForTimeout(1000);
@@ -281,21 +268,17 @@ const doTask = async (page, task, tags) => {
                 const text = chooseRandomTag(tags, tagCount) + " ";
                 actions.push('tag')
 
-                if (await page.waitForSelector('[aria-label="Reply"]', { visible: true, timeout: 10000 }).catch((e) => { return 'error' }) === 'error') {
+                if (await page.click('[aria-label="Reply"]').catch((e) => { return 'error' }) === 'error') {
                     fails.push('tag');
                 } else {
-                    if (await page.click('[aria-label="Reply"]').catch((e) => { return 'error' }) === 'error') {
+                    if (await page.waitForSelector('[role="dialog"] [aria-label="Tweet text"]', { visible: true, timeout: 20000 }).catch((e) => { return 'error' }) === 'error') {
                         fails.push('tag');
                     } else {
-                        if (await page.waitForSelector('[role="dialog"] [aria-label="Tweet text"]', { visible: true, timeout: 10000 }).catch((e) => { return 'error' }) === 'error') {
-                            fails.push('tag');
-                        } else {
-                            await page.waitForTimeout(1000);
-                            await page.keyboard.type(text, { delay: 100 });
-                            await page.waitForTimeout(500);
-                            await page.click('[role="dialog"] [data-testid="toolBar"] [data-testid="tweetButton"]');
-                            await page.waitForTimeout(1000);
-                        }
+                        await page.waitForTimeout(1000);
+                        await page.keyboard.type(text, { delay: 100 });
+                        await page.waitForTimeout(500);
+                        await page.click('[role="dialog"] [data-testid="toolBar"] [data-testid="tweetButton"]');
+                        await page.waitForTimeout(1000);
                     }
                 }
                 await page.waitForTimeout(1000);
